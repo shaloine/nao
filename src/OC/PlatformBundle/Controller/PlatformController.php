342fc64c22b4481b5495ee1bf17fb0bf86846d39
shaloine <shaloine2@hotmail.com>
2017-09-12 11:14:01 +0200

    mise en place routing annontation

--- src/OC/PlatformBundle/Controller/PlatformController.php
+++ src/OC/PlatformBundle/Controller/PlatformController.php
@@ -4,18 +4,25 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use OC\PlatformBundle\Entity\Article;
 use OC\PlatformBundle\Entity\User;
 use OC\PlatformBundle\Form\ArticleType;
 
 class PlatformController extends Controller
-{
+{	
+	/**
+     * @Route("/", name="oc_platform_homepage")
+     */
 	public function indexAction()
 	{
 		return $this->render('OCPlatformBundle:Default:index.html.twig');
 	}
 
+	/**
+     * @Route("/creation", name="oc_platform_creation")
+     */
 	public function creationAction(Request $request)
 	{
 		$article = new Article();
