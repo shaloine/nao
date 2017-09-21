f56e19e34307222bad1e11ae66c060c521931a7e
shaloine <shaloine2@hotmail.com>
2017-09-21 15:57:00 +0200

    vue inscription + captcha

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new OC\PlatformBundle\OCPlatformBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Vihuvac\Bundle\RecaptchaBundle\VihuvacRecaptchaBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
