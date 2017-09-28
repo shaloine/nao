27add005cfa028ada129fa8395335beaae9e7b62
Fabriel <ocr@loubier.fr>
2017-09-28 13:24:09 +0200

    Ajout texte naturalistes formulaire login et corrections diverses

---
+++ web/js/naturalist.js
@@ -0,0 +1,7 @@
+function display() {
+    if(document.getElementById('fos_user_registration_form_naturalist').checked) {
+        document.getElementById('diplom').style.display = 'block';
+    } else {
+        document.getElementById('diplom').style.display = 'none';
+    }
+}
